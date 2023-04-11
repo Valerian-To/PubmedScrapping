bec11acd94846140091af6b378fb3d12c1db3683
Valerian-To <valerian.touzard@semiir.com>
2023-04-11 14:07:21 +0200

    feat: AJout du dotenv

--- bdd/parameter.py
+++ bdd/parameter.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
 
-
+loginDb = os.getenv('LOGIN_DB')
+passwordDb = os.getenv('PASSWORD_DB')
 Base = declarative_base()
-engine = create_engine('mysql+mysqlconnector://root:root@localhost/pubmed', echo=True)
+engine = create_engine('mysql+mysqlconnector://'+loginDb+':'+passwordDb+'@localhost/pubmed', echo=True)
 Session = sessionmaker()
